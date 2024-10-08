79b250b58ade149c9f1820889cdfd223b91dc697
bendoz0 <147492032+bendoz0@users.noreply.github.com>
2024-10-08 13:31:19 +0200

    Struttura login.

--- backend/src/main/java/it/itsrizzoli/backend/controllers/UserController.java
+++ backend/src/main/java/it/itsrizzoli/backend/controllers/UserController.java
@@ -3,13 +3,43 @@ import it.itsrizzoli.backend.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
-    @PostMapping("/login")
-    public String login(Model model){
+
+    @GetMapping(value="/login")
+    public String ShowLogin(Model model) {
         model.addAttribute("user", new User());
-        return "index";
+        return "login";
+    }
+
+
+    @PostMapping("/login")
+    public String SubmitLogin(@ModelAttribute User user, Model model){
+        model.addAttribute("user", user);
+        String email = user.getEmail().trim();    //.trim() restituisce la stringa togliendo gli spazi sia all'inizio che fine
+        String password = user.getPassword().trim();
+        boolean stato = true;
+
+        //al posto dei controlli sottostanti, fare query al DB per verificare credenziali
+
+        /* \\p{L} consente tutte le lettere anche accentate
+           \\s    consente gli spazi
+         */
+        if(email.isEmpty() || !email.matches("[\\p{L}\\s]+")) {
+            model.addAttribute("errorEmail", "Email inserita non valida");
+            stato = false;
+        }
+        if(password.isEmpty() || !password.matches("[\\p{L}\\s]+")) {
+            model.addAttribute("errorPassword", "Password errata");
+            stato = false;
+        }
+        if(!stato) {
+            return "login";
+        }
+
+        return "homePage";
     }
 }
