f20adf6bfa106b3bf2d0cd3b546f54740ea5f481
Luca Ilari <luca.ilari@gmail.com>
2024-10-08 13:00:45 +0200

    To fix

--- backend/src/main/java/it/itsrizzoli/backend/controllers/UserController.java
+++ backend/src/main/java/it/itsrizzoli/backend/controllers/UserController.java
@@ -1,4 +1,15 @@
 package it.itsrizzoli.backend.controllers;
+import it.itsrizzoli.backend.models.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class UserController {
+    @PostMapping("/login")
+    public String login(Model model){
+        model.addAttribute("user", new User());
+        return "index";
+    }
 }
--- backend/src/main/java/it/itsrizzoli/backend/models/User.java
+++ backend/src/main/java/it/itsrizzoli/backend/models/User.java
@@ -1,5 +1,22 @@
 package it.itsrizzoli.backend.models;
 
 public class User {
+    private String email;
+    private String password;
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
